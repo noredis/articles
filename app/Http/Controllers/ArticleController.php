fa05713fac5badf3e05f8297f8981f30a29bf052
Rustam Urazov <noredis@yandex.ru>
2026-02-18 18:29:12 +0600

    complete POST /api/articles method

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\StoreArticle;
 use App\Http\Requests\StoreArticleRequest;
-use Illuminate\Http\Response;
+use App\Http\Resources\ArticleResource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class ArticleController extends Controller
 {
-    public function store(StoreArticleRequest $request): Response
+    public function store(
+        StoreArticleRequest $request,
+        StoreArticle $storeArticle,
+    ): JsonResource
     {
-        return new Response();
+        $article = $storeArticle($request->validated());
+        return new ArticleResource($article);
     }
 }
