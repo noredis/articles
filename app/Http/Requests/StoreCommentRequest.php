1c46b7420d653349cb3aa6b95a4334eec0bad494
Rustam Urazov <noredis@yandex.ru>
2026-02-18 20:07:07 +0600

    create store comment request

---
+++ app/Http/Requests/StoreCommentRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'article_id'  => ['required', 'integer'],
+            'author_name' => ['required', 'string', 'max:127'],
+            'content'     => ['required', 'string', 'max:1000'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'article_id.required'  => 'article_id is required',
+            'article_id.integer'   => 'article_id must be an integer',
+            'author_name.required' => 'author_name is required',
+            'author_name.string'   => 'author_name must be a string',
+            'author_name.max'      => 'author_name must not exceed 127 characters',
+            'content.required'     => 'content is required',
+            'content.string'       => 'content must be a string',
+            'content.max'          => 'content must not exceed 1000 characters',
+        ];
+    }
+}
