e4da1c0f9531c3466e45ddca76ed0d586363b3ca
Rustam Urazov <noredis@yandex.ru>
2026-02-19 14:51:33 +0600

    complete POST /api/articles/{id}/comments method

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\StoreComment;
+use App\Http\Requests\StoreCommentRequest;
+use App\Http\Resources\CommentResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
 class CommentController extends Controller
 {
-    public function store()
+    public function store(
+        StoreCommentRequest $request,
+        StoreComment $storeComment,
+    ): JsonResource
     {
+        $comment = $storeComment($request->validated());
+        return new CommentResource($comment);
     }
 }
