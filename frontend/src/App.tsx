f0bfe935993868f9a3247ae3e6be9f91ec800b49
Rustam Urazov <noredis@yandex.ru>
2026-02-20 18:26:16 +0600

    navigation to details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './common/ui/Header';
 import { ArticleList } from './feature/ArticleList';
+import { ArticleDetails } from './feature/ArticleDetails';
 
 function App() {
   return (
-    <>
-      <Header/>
-      <ArticleList/>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/articles/:id" element={<ArticleDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/entity/ui/Article.tsx
+++ frontend/src/entity/ui/Article.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { truncate } from "../../common/utils/truncate-string";
 import { Article } from "../model/article";
 
@@ -6,13 +7,19 @@ type ArticleProps = {
 };
 
 export const ArticleView = ({article}: ArticleProps) => {
+    const navigate = useNavigate();
+    
+    const onOpenDetails = (id: number) => {
+        navigate(`/articles/${id}`);
+    };
+    
     return (
         <div className="article-container">
             <div className="article-title">
             {article.title}
             </div>
             {truncate(article.content, 400)}
-            <div className="article-read-completely button">
+            <div className="article-read-completely button" onClick={() => {onOpenDetails(article.id)}}>
                 Читать полностью
             </div>
         </div>
